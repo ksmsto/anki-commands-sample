88f7e4a781333907ec6d0d8dfab9c32def8dfee4
masato_kasa <masato_kasa@uni0069n.local>
2024-02-23 16:01:40 +0900

    外公開用の内容にやや変更(deck名とか諸々かな)

--- create-index.js
+++ create-index.js
@@ -1,127 +1,168 @@
+// ankiのport(デフォルト)
+const URL = "http://localhost:8765";
 
-const URL = 'http://localhost:8765'
+const VERSION = 6;
 
-const VERSION = 6
+// 使用しているメソッド
 const METHODS = {
-    "getCard": "cardsInfo",
-    "cardsToNotes": "cardsToNotes",
-    "updateNote": "updateNote",
-    "notesInfo": "notesInfo",
-    "fetchDeck": "findCards",
-    "addTags": "addTags",
-    "removeTags": "removeTags",
-}
+  getCard: "cardsInfo",
+  cardsToNotes: "cardsToNotes",
+  updateNote: "updateNote",
+  notesInfo: "notesInfo",
+  fetchDeck: "findCards",
+  addTags: "addTags",
+  removeTags: "removeTags",
+};
 
+// 使用しているタグ
 const TAGS = {
-    "current": "current",
-    "active": "active"
-}
+  current: "current",
+  active: "active",
+};
 
+// sleep関数
 const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
+// indexの配列からsort用のkeyを作成
+// getSortByIndexKey(createSortByIndex(range(1, 10))) -> [{key: 'index', value: 1}, {key: 'index', value: 2}, ...]
+const createSortByIndex = (indexArr) =>
+  indexArr.map((v) => ({ key: "index", value: v }));
+
+// type SortKeys = {
+//    key: 'index',
+//   value: number
+// }
+// filter用のkeyを作成
+const getSortByIndexKey = (key) => {
+  return `(${key.map((v) => `${v.key}:${v.value}`).join(" OR ")})`;
+};
+
+// methodのbodyのベース
 const getBaseMethod = (method) => ({
-        action: METHODS[method],
-        version: VERSION,
-})
+  action: METHODS[method],
+  version: VERSION,
+});
+// METHODSまとめ
 const METHODS_PARAMS = {
-    "getCard": (id) => ({
-        ...getBaseMethod('getCard'),
-        params: {
-            cards: [id]
-        }
-    }),
-    "cardsToNotes": (ids) => ({
-        ...getBaseMethod('cardsToNotes'),
-        params: {
-            cards: ids
-        }
-    }),
-    "notesInfo": (id) => ({
-        ...getBaseMethod('notesInfo'),
-        params: {
-            notes: [id]
-        }
-    }),
-    "updateNote": (data, index, tags = []) => ({
-        ...getBaseMethod('updateNote'),
-        params: {
-            note: {
-                id: data.noteId,
-                fields: {
-                    Front: `${data.fields.Front.value}`,
-                    Back: `${data.fields.Back.value}`,
-                    index: `${index}` 
-                },
-                tags: data.tags
-            }
-        }
-    }),
-    "fetchDeck":  (deck, query = '') => ({
-        ...getBaseMethod('fetchDeck'),
-        params: {
-            query: `deck:${deck}${query}`
-        }
-    }),
-    "addTags": (id, tag) => ({
-        ...getBaseMethod('addTags'),
-        params: {
-            notes: [id],
-            tags: TAGS[tag]
-        }
-    }),
-    "removeTags": (id, tag) => ({
-        ...getBaseMethod('removeTags'),
-        params: {
-            notes: [id],
-            tags: TAGS[tag]
-        }
-    }),
-}
-
-const fetcher = async(reqBody) => {
-    return await fetch(URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
+  // card取得
+  getCard: (id) => ({
+    ...getBaseMethod("getCard"),
+    params: {
+      cards: [id],
+    },
+  }),
+  // card -> note情報取得
+  cardsToNotes: (ids) => ({
+    ...getBaseMethod("cardsToNotes"),
+    params: {
+      cards: ids,
+    },
+  }),
+  // note情報取得
+  notesInfo: (id) => ({
+    ...getBaseMethod("notesInfo"),
+    params: {
+      notes: [id],
+    },
+  }),
+  // note更新
+  updateNote: (data, updateFields = {}, tags = []) => ({
+    ...getBaseMethod("updateNote"),
+    params: {
+      note: {
+        id: data.noteId,
+        fields: {
+          Front: `${data.fields.Front.value}`,
+          Back: `${data.fields.Back.value}`,
+          index: `${data.fields.index.value}`,
+          ...updateFields,
         },
-        body: JSON.stringify(reqBody)
-    }).then(res => res.json()).catch((err) => console.log(`error: ${err}`))
-}
+        tags: tags,
+      },
+    },
+  }),
+  // deckからカード取得
+  fetchDeck: (deck, query = "") => ({
+    ...getBaseMethod("fetchDeck"),
+    params: {
+      query: `deck:${deck}${query}`,
+    },
+  }),
+  // noteにタグ追加
+  addTags: (id, tag) => ({
+    ...getBaseMethod("addTags"),
+    params: {
+      notes: [id],
+      tags: TAGS[tag],
+    },
+  }),
+  // noteにタグ削除
+  removeTags: (id, tag) => ({
+    ...getBaseMethod("removeTags"),
+    params: {
+      notes: [id],
+      tags: TAGS[tag],
+    },
+  }),
+};
 
+// API fetcher
+const fetcher = async (reqBody) => {
+  return await fetch(URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(reqBody),
+  })
+    .then((res) => res.json())
+    .catch((err) => console.log(`error: ${err}`));
+};
+
+// range: (start: number, end: number) => number[]
 const range = (start, end) => {
-    // 結果を格納するための配列を初期化
-    let result = [];
+  const result = [];
+  for (let i = start; i < end; i += 1) {
+    result.push(i);
+  }
+  return result;
+};
 
-        for (let i = start; i < end; i += 1) {
-            result.push(i);
-        }
-    return result;
-}
+// sample codes
+const main = async () => {
+  // 対象は1 ~ 100番目のカード
+  const rangeInfo = range(1, 100);
+  // sampleのデッキからカードを取得
+  const { result: cardIds } = await fetcher(
+    METHODS_PARAMS["fetchDeck"](
+      "sample",
+      " " + getSortByIndexKey(createSortByIndex(rangeInfo))
+    )
+  );
+  // カードからノートIDを取得
+  const { result: noteIds } = await fetcher(
+    METHODS_PARAMS["cardsToNotes"](cardIds)
+  );
 
-const main = async() => {
-    const t= range(1, 750)
-    const {result: cardIds} = await fetcher(METHODS_PARAMS['fetchDeck']('words:levelB', ' tag:current'))
-    // const {result: cardIds} = await fetcher(METHODS_PARAMS['fetchDeck']('sample'))
-    console.log(cardIds)
-    const {result: noteIds} = await fetcher(METHODS_PARAMS['cardsToNotes'](cardIds))
-    console.log(noteIds)
-    if(!noteIds) {
-        console.log("Error: No Note Id")
-        return
-    }
+  if (!noteIds) {
+    console.log("Error: No Note Id");
+    return;
+  }
 
-    for (const [i, v] of noteIds.entries())  {
-        await sleep(25)
-        const noteDetail = await fetcher(METHODS_PARAMS['notesInfo'](v))
-        if(!noteDetail) return
-        console.log(noteDetail.result[0].fields)
-        // const res = await fetcher(METHODS_PARAMS['updateNote'](noteDetail.result[0], i + 1))
-        // if(Number(noteDetail.result[0].fields.index.value) !== i + 1) console.log(i + 1, 'change')
-        // console.log(noteDetail.result[0].fields.index.value, i + 1)
-    }
-}
+  // ノートIDからノート情報を取得し、indexを更新
+  for (const [i, v] of noteIds.entries()) {
+    // 25ms待機: 一気に処理させるとエラーが出るため
+    await sleep(25);
+    // ノート情報取得
+    const noteDetail = await fetcher(METHODS_PARAMS["notesInfo"](v));
+    if (!noteDetail) return;
+    // update処理: 第二引数、第三引数は任意になっているが、そこに更新したい内容を入れる
+    const res = await fetcher(
+      METHODS_PARAMS["updateNote"](noteDetail.result[0], {}, [])
+    );
+  }
+};
 
-void main()
-
+void main();
